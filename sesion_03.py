660d1b8f33da3fe40e2c1d2c365477f624be39d3
BryanRosas1209 <bryan.rosaaas@gmail.com>
2026-02-12 17:27:46 -0800

    Ultimo cambio en sesion 03

--- sesion_03.py
+++ sesion_03.py
@@ -1,5 +1,6 @@
 #loops
 
+
 mi_lista = {1,2,3,4}
 
 for i in mi_lista:
@@ -13,8 +14,30 @@ print(f"El resultado de la sume de mi lista es: {resultado}")
 for i in range(2, 8):
      print(i)
 
-mi_lista_2 = {"lunes","martes","miercoles","jueves","viernes"}
+mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
 
-for i in mi_lista_2:
-     if i != "lunes":
-        print(f"Feliz: {i}" )
+contador = 0
+
+while contador < 3:
+    for i in mi_lista_2:
+        if i != "lunes":
+            print(i)
+    contador += 1
+
+
+# While loop
+
+
+# Practica 2
+# Dada la lista mi_lista_2 = {"lunes","martes","miercoles","jueves","viernes"}
+# Imprimer Cada ekemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# Pista usa los dos tupos loops while y for en ek mismo programa ára lograrlo 
+# Resultado
+# Martes
+# Miercoles
+# jueves
+# viernes
+# martes 
+# miiercoles 
+# jueves
+# viernes
